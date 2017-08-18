0b1e365462b0ec2437f8ce4eaa3c5ceeb9c0481c
Natalia Mecner <natalia.mecner@gmail.com>
2017-08-18 20:49:59 +0200

    Working simple version, without databases

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [
+
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'lorem',
+            'author': 'Natalia Mecner',
+            'create_date': '17-08-2017'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'I am getting better',
+            'author': 'Natalia Mecner',
+            'create_date': '17-08-2017'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': "I'm even more into it",
+            'author': 'Natalia Mecner',
+            'create_date': '17-08-2017'
+        }
+    ]
+    return articles
